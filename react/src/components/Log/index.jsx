a7cbbf40e9bfb819a7f33629ef13de6f1ea8d74b
Sean Polidori <seanpolidori0@gmail.com>
2023-08-04 18:56:33 -0400

    Defined Log's general layout

--- react/src/components/Log/index.jsx
+++ react/src/components/Log/index.jsx
@@ -1,5 +1,7 @@
 /* eslint-disable react/prop-types */
-import { tabType } from "../../enums/tabType"
+import Icon from "../Icon";
+import iconType from "../../enums/iconType";
+import tabType from "../../enums/tabType"
 import style from './style.module.css'
 
 const Log = ({type}) => {
@@ -10,12 +12,46 @@ const Log = ({type}) => {
             break;
     }
 
+    let logs = [];
+    for (let i = 0; i < 100; i++) {
+        logs.push(<li></li>);
+    }
+
     return (
         <section className={style.container}>
             <form>
-                <input type='text' placeholder='Search'/>
+                <SearchBar />
+                <a href="#">
+                    <Icon type={iconType.FILTER} />
+                </a>
+                <a href="#">
+                    <Icon type={iconType.SORT} />
+                </a>
             </form>
+            <Scrollpane>
+                <ul>
+                    {logs}
+                </ul>
+            </Scrollpane>
         </section>
+    )
+}
+
+const SearchBar = () => {
+    return (
+        <div className={style.searchBar}>
+            <input type='text' placeholder='Search'/>
+            <Icon type={iconType.SEARCH} />
+        </div>
+        
+    )
+}
+
+const Scrollpane = ({children}) => {
+    return (
+        <div className={style.scrollpane}>
+            {children}
+        </div>
     )
 }
 
