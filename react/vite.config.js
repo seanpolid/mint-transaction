35450d1999077f396ae66668b5981c648b178f96
Sean Polidori <seanpolidori0@gmail.com>
2023-08-13 20:30:43 -0400

    Tweaked file formatting

--- react/vite.config.js
+++ react/vite.config.js
@@ -3,8 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-	  outDir: "../src/main/resources/static"
-  }
+    plugins: [react()],
+    build: {
+        outDir: "../src/main/resources/static"
+    }
 })
