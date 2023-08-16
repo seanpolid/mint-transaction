78a4e94725a6e70142f0a7363cefa4920947db6e
Sean Polidori <seanpolidori0@gmail.com>
2023-08-15 21:11:12 -0400

    Made components for select

---
+++ react/src/components/SelectWithItems/index.jsx
@@ -0,0 +1,24 @@
+/* eslint-disable react/prop-types */
+/**
+ * Creates a select box with a set of options specified through the provided items. 
+ * 
+ * @param param0 
+ * @returns 
+ */
+const SelectWithItems = ({id, name, items, value, onChange, selectRef}) => {
+    let selectValue = undefined;
+    if (onChange) {
+        selectValue = value ? value : '';
+    } 
+
+    return (
+        <select id={id} name={name} onChange={onChange} value={selectValue} ref={selectRef}>
+            <option hidden>-- Choose option --</option>
+            {items && items.map(item => (
+                <option key={item.id} value={item.id}>{item.name}</option>)
+            )}
+        </select>
+    )
+}
+
+export default SelectWithItems;
---
+++ react/src/components/SelectWithLabel/index.jsx
@@ -0,0 +1,34 @@
+/* eslint-disable react/prop-types */
+import SelectWithItems from "../SelectWithItems";
+
+const SelectWithLabel = ({id, name, text, items, value, onChange, wrapped=true}) => {
+    return (
+        <>
+            {wrapped ? (
+                <label>
+                    {text}
+                    <SelectWithItems 
+                        id={id} 
+                        name={name} 
+                        items={items}
+                        value={value}
+                        onChange={onChange} 
+                    />
+                </label>
+            ) : (
+                <>
+                    <label htmlFor={id}>{text}</label> 
+                    <SelectWithItems 
+                        id={id}
+                        name={name}
+                        items={items}
+                        value={value}
+                        onChange={onChange}
+                    />
+                </>
+            )}
+        </>
+    )
+}
+
+export default SelectWithLabel;
