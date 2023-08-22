7a079a126bf7c910ea3af6e679ca18daa4ae19de
Sean Polidori <seanpolidori0@gmail.com>
2023-08-21 23:36:21 -0400

    Made a model for Category, updated Transaction, and exposed all models through index

--- react/src/models/Transaction.js
+++ react/src/models/Transaction.js
@@ -1,4 +1,5 @@
 class Transaction {
+    id;
     type;
     category;
     recurs;
@@ -7,6 +8,20 @@ class Transaction {
     amount;
     notes;
     key;
+
+    clone() {
+        const transaction = new Transaction();
+        transaction.id = this.id;
+        transaction.type = this.type;
+        transaction.category = this.category;
+        transaction.recurs = this.recurs;
+        transaction.startDate = this.startDate;
+        transaction.endDate = this.endDate;
+        transaction.amount = this.amount;
+        transaction.notes = this.notes;
+        transaction.key = this.key;
+        return transaction;
+    }
 }
 
 export default Transaction
---
+++ react/src/models/index.jsx
@@ -0,0 +1,7 @@
+import Category from "./Category";
+import Goal from "./Goal";
+import Transaction from "./Transaction";
+import User from './User';
+
+
+export { Category, Goal, Transaction, User}
