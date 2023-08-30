5270f8ea35435e0caa695ab4e0b9b18da2f1247f
Sean Polidori <seanpolidori0@gmail.com>
2023-08-30 18:32:41 -0400

    Updated entities

--- src/main/java/application/entities/User.java
+++ src/main/java/application/entities/User.java
@@ -1,8 +1,15 @@
 package application.entities;
 
 import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -12,13 +19,15 @@ import jakarta.persistence.Table;
 
 @Entity
 @Table(name="user")
-public class User {
+public class User implements UserDetails {
 
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private int id;
 	
 	private String email;
+	
+	private String username;
 	
 	private String firstName;
 	
@@ -28,10 +37,10 @@ public class User {
 	
 	private long phone;
 	
-	@OneToMany
+	@OneToMany(mappedBy="user", cascade=CascadeType.ALL)
 	private List<Transaction> transactions;
 	
-	@OneToMany
+	@OneToMany(mappedBy="user", cascade=CascadeType.ALL)
 	private List<Goal> goals;
 
 	public int getId() {
@@ -48,6 +57,10 @@ public class User {
 
 	public void setEmail(String email) {
 		this.email = email;
+	}
+	
+	public void setUsername(String username) {
+		this.username = username;
 	}
 
 	public String getFirstName() {
@@ -96,5 +109,35 @@ public class User {
 
 	public void setGoals(List<Goal> goals) {
 		this.goals = goals;
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return Arrays.asList(new SimpleGrantedAuthority("ROLE_USER"));
+	}
+
+	@Override
+	public String getUsername() {
+		return username;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
 	}
 }
