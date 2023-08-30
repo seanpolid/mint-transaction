4198f0bdce3058a5619598f0b5336203cac96496
Sean Polidori <seanpolidori0@gmail.com>
2023-08-30 18:42:27 -0400

    Added ability to save and retrieve transactions

--- src/main/java/application/repositories/ITransactionRepository.java
+++ src/main/java/application/repositories/ITransactionRepository.java
@@ -1,9 +1,13 @@
 package application.repositories;
 
+import java.util.List;
+
 import org.springframework.data.repository.ListCrudRepository;
 
 import application.entities.Transaction;
 
 public interface ITransactionRepository extends ListCrudRepository<Transaction, Integer> {
 
+	public List<Transaction> findAllByUserId(int userId);
+	
 }
