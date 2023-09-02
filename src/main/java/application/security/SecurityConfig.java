b23471e53415fcb0b2d558f5c343c5ba64cc9d20
Sean Polidori <seanpolidori0@gmail.com>
2023-09-01 22:42:45 -0400

    disabled security to work on endpoints

--- src/main/java/application/security/SecurityConfig.java
+++ src/main/java/application/security/SecurityConfig.java
@@ -2,11 +2,13 @@ package application.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 import application.entities.User;
 import application.repositories.IUserRepository;
@@ -30,4 +32,12 @@ public class SecurityConfig {
 		};
 	}
 	*/
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		return http.authorizeHttpRequests((customizer) -> {
+			customizer.anyRequest().permitAll();
+		}).csrf((customizer) -> {
+			customizer.disable();
+		}).build();
+	}
 }
