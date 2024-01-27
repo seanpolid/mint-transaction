b597dc88599b4fa7e8b963f75ddd363d08867f25
seanpolid <seanpolidori0@gmail.com>
2024-01-27 18:29:09 -0500

    Removed forced loading

--- react/src/stores/StatusContextProvider.jsx
+++ react/src/stores/StatusContextProvider.jsx
@@ -1,14 +1,9 @@
-import { useEffect } from "react";
 import StatusContext from "./StatusContext";
 import { useState } from "react";
 
 const StatusContextProvider = ({children}) => {
-    const [loadingData, setLoadingData] = useState(1);
+    const [loadingData, setLoadingData] = useState(0);
     const [isNetworkError, setIsNetworkError] = useState(false);
-
-    useEffect(() => {
-        setTimeout(() => setLoadingData(prevLoadingData => prevLoadingData - 1), 4000);   
-    }, [])
 
     const incrementLoadingData = () => {
         setLoadingData(prevLoadingData => prevLoadingData + 1);
