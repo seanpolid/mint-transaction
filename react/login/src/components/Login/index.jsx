5391939b17892ec90dcfe9c00fff04516365ae6d
seanpolid <seanpolidori0@gmail.com>
2024-02-20 18:08:46 -0500

    Made registration its own component and tested it

--- react/login/src/components/Login/index.jsx
+++ react/login/src/components/Login/index.jsx
@@ -1,6 +1,7 @@
 import githubIcon from '../../assets/github-mark.svg'
 import googleIcon from '../../assets/google-icon.svg'
-import React, { useCallback, useState, useRef } from "react";
+import React, { useState } from "react";
+import Registration from '../Registration';
 
 let host;
 if (import.meta.env.DEV) {
@@ -88,108 +89,6 @@ const Overlay = ({onClick}) => {
     return (
         <div className='overlay' onClick={onClick}></div>
     )
-}
-
-const Registration = ({handleRegistration}) => {
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
-    const [email, setEmail] = useState("");
-    const [phone, setPhone] = useState("");
-    const [error, setError] = useState("");
-    const emailRef = useRef();
-
-    const inputs = [
-        {name: "username", placeholder: "Username", type: "text", value: username},
-        {name: "password", placeholder: "Password", type: "password", autoComplete: "new-password", value: password},
-        {name: "email", placeholder: "Email", type: "email", value: email, ref: emailRef},
-        {name: "phone", placeholder: "Phone", type: "tel", value: phone},
-    ]
-
-    const setters = {
-        "username": setUsername,
-        "password": setPassword,
-        "email": setEmail,
-        "phone": setPhone
-    }
-
-    const handleChange = (event) => {
-        const name = event.target.name;
-        const value = event.target.value;
-        setters[name](value);
-    }
-
-    const handleSubmit = useCallback(async (event) => {
-        event.preventDefault();
-
-        const body = {
-            "username": username,
-            "password": password,
-            "email": email,
-            "phone": phone === "" ? null : phone
-        }
-
-        const string = isValid(body, emailRef);
-        if (string) {
-            setError(string);
-            return;
-        }
-        
-        const response = await fetch(`${host}/api/user`, {
-            method: "POST",
-            headers: {
-                "Accept": "application/json",
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(body)
-        });
-        
-        if (response.status >= 400) {
-            const responseBody = await response.json();
-            setError(responseBody.message);
-            return;
-        }
-
-        handleRegistration(username, password);
-    }, [username, password, email, phone]);
-
-    return (
-        <form className='modal'>
-            {inputs.map(input => (
-                <input key={input.name} {...input} onChange={handleChange}/>
-            ))}
-
-            <input 
-                type="submit" 
-                name="submit" 
-                value="Register"
-                onClick={handleSubmit}
-            />
-
-            {<p className='error' hidden={error.length == 0}>Error: {error}</p>}
-        </form>
-    )
-}
-
-function isValid(body, emailRef) {
-    if (body.username.length === 0) {
-        return "Please provide a username.";
-    } 
-    if (body.password.length === 0) {
-        return "Please provide a password.";
-    }
-    if (body.email.length === 0 || !emailRef.current.validity.valid) {
-        return "Please provide a valid email.";
-    }
-    if (body.phone && body.phone.length > 0 && !isValidPhoneNumber(body.phone)) {
-        return "Please provide a valid phone number.";
-    }
-
-    return null;
-}
-
-function isValidPhoneNumber(phone) {
-    const pattern = /^\(?([0-9]{3})\)?[-.●]?([0-9]{3})[-.●]?([0-9]{4})$/;
-    return pattern.test(phone);
 }
 
 export default Login
---
+++ react/login/src/components/Registration/index.test.jsx
@@ -0,0 +1,221 @@
+import { beforeEach, describe, test, expect, afterEach, vi } from "vitest";
+import Registration from '.';
+import { rest } from 'msw';
+import { setupServer } from 'msw/node';
+import { screen, render, cleanup, waitFor  } from "@testing-library/react";
+import userEvent from '@testing-library/user-event';
+
+const successHandlers = [
+    rest.post('http://localhost:8080/api/user', (req, res, ctx) => {
+        return res(ctx.status(204), null);
+    })
+]
+
+const failureHandlers = [
+    rest.post('http://localhost:8080/api/user', (req, res, ctx) => {
+        return res(ctx.status(400), {message: ""})
+    })
+]
+
+describe('Registration', () => {
+
+    let registration;
+    let server;
+    beforeEach(() => {
+        registration = <Registration handleRegistration={() => {}} />;
+    })
+
+    afterEach(() => {
+        cleanup();
+
+        if (server) {
+            server.close();
+        }
+    })
+
+    test('should render', () => {
+        // Act
+        render(registration);
+
+        // Assert
+        expect(screen.queryByPlaceholderText('Username')).not.toBeNull();
+        expect(screen.queryByPlaceholderText('Password')).not.toBeNull();
+        expect(screen.queryByPlaceholderText('Email')).not.toBeNull();
+        expect(screen.queryByPlaceholderText('Phone')).not.toBeNull();
+        expect(screen.queryByRole('button')).not.toBeNull(); 
+    })
+
+    test('should handle username change', async () => {
+        // Arrange
+        const user = userEvent.setup();
+        render(registration);
+        
+        const username = 'username';
+        
+        // Act
+        await user.type(screen.getByPlaceholderText('Username'), username);
+
+        // Assert
+        expect(screen.getByPlaceholderText('Username').value).toBe(username);
+    })
+
+    test('should handle password change', async () => {
+        // Arrange
+        const user = userEvent.setup();
+        render(registration);
+        const password = 'password';
+
+        // Act
+        await user.type(screen.getByPlaceholderText('Password'), password);
+
+        // Assert
+        expect(screen.getByPlaceholderText('Password').value).toBe(password);
+    })
+
+    test('should handle email change', async () => {
+        // Arrange
+        const user = userEvent.setup();
+        render(registration);
+
+        const email = 'email@gmail.com';
+
+        // Act
+        await user.type(screen.getByPlaceholderText('Email'), email);
+
+        // Assert
+        expect(screen.getByPlaceholderText('Email').value).toBe(email);
+    })
+
+    test('should handle phone change', async () => {
+        // Arrange
+        const user = userEvent.setup();
+        render(registration);
+
+        const phone = '7341112222';
+
+        // Act
+        await user.type(screen.getByPlaceholderText('Phone'), phone);
+
+        // Assert
+        expect(screen.getByPlaceholderText('Phone').value).toBe(phone);
+    })
+
+    test('should handle empty form submission', async () => {
+        // Arrange
+        const user = userEvent.setup();
+        render(registration);
+
+        // Act
+        await user.click(screen.getByRole('button'));
+
+        // Assert
+        expect(screen.queryByText('provide a username', {exact: false})).not.toBeNull();
+    })
+    
+    test('should handle form submission with only username', async () => {
+        // Arrange
+        const user = userEvent.setup();
+        render(registration);
+
+        // Act
+        await user.type(screen.getByPlaceholderText('Username'), 'username');
+        await user.click(screen.getByRole('button'));
+
+        // Assert
+        expect(screen.queryByText('provide a password', {exact: false})).not.toBeNull();
+    })
+    
+    test('should handle form submission with only username and password', async () => {
+        // Arrange
+        const user = userEvent.setup();
+        render(registration);
+
+        await user.type(screen.getByPlaceholderText('Username'), 'username');
+        
+        // Act
+        await user.type(screen.getByPlaceholderText('Password'), 'password');
+        await user.click(screen.getByRole('button'));
+
+        // Assert
+        expect(screen.queryByText('provide a valid email', {exact: false})).not.toBeNull();
+    })
+
+    test('should handle invalid email', async () => {
+        // Arrange
+        const user = userEvent.setup();
+        render(registration);
+
+        await user.type(screen.getByPlaceholderText('Username'), 'username');
+        await user.type(screen.getByPlaceholderText('Password'), 'password');
+
+        // Act
+        await user.type(screen.getByPlaceholderText('Email'), 'email');
+        await user.click(screen.getByRole('button'));
+
+        // Assert
+        expect(screen.queryByText('provide a valid email', {exact: false})).not.toBeNull();
+    })
+
+    test('should handle registration error', async () => {
+        // Arrange
+        const mockFunctions = {
+            handleRegistration: (username, password) => [username, password]
+        }
+        const spy = vi.spyOn(mockFunctions, 'handleRegistration');
+        registration = <Registration handleRegistration={mockFunctions.handleRegistration} />
+
+        const errorMessage = "Email in use.";
+        const postAPI = rest.post('http://localhost:8080/api/user', (req, res, ctx) => {
+            return res(ctx.status(400), ctx.json({message: errorMessage}));
+        })
+        server = setupServer(postAPI);
+        server.listen();
+
+        const user = userEvent.setup();
+        render(registration);
+
+        await user.type(screen.getByPlaceholderText('Username'), 'username');
+        await user.type(screen.getByPlaceholderText('Password'), 'password');
+        await user.type(screen.getByPlaceholderText('Email'), 'email@gmail.com');
+
+        // Act
+        await user.click(screen.getByRole('button'));
+
+        // Assert   
+        await waitFor(() => {
+            expect(spy).not.toHaveBeenCalledOnce();
+            expect(screen.queryByText('Email in use.', {exact: false})).not.toBeNull();
+        })
+    })
+
+    test('should handle registration success', async () => {
+        // Arrange
+        const mockFunctions = {
+            handleRegistration: (username, password) => [username, password]
+        }
+        const spy = vi.spyOn(mockFunctions, 'handleRegistration');
+        registration = <Registration handleRegistration={mockFunctions.handleRegistration} />
+
+        const postAPI = rest.post('http://localhost:8080/api/user', (req, res, ctx) => {
+            return res(ctx.status(200), null);
+        })
+        server = setupServer(postAPI);
+        server.listen();
+
+        const user = userEvent.setup();
+        render(registration);
+
+        await user.type(screen.getByPlaceholderText('Username'), 'username');
+        await user.type(screen.getByPlaceholderText('Password'), 'password');
+        await user.type(screen.getByPlaceholderText('Email'), 'email@gmail.com');
+
+        // Act
+        await user.click(screen.getByRole('button'));
+
+        // Assert   
+        await waitFor(() => {
+            expect(spy).toHaveBeenCalledOnce();
+        })
+    })
+
+})
