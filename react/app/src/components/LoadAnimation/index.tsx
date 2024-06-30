a811ec248d6b85bb3b97b6cbe62c3edc58ed4a2b
seanpolid <seanpolidori0@gmail.com>
2024-06-29 22:45:36 -0400

    made loading animation more modern

--- react/app/src/components/LoadAnimation/index.tsx
+++ react/app/src/components/LoadAnimation/index.tsx
@@ -1,32 +1,10 @@
-import { useState, useEffect } from "react";
-
 import style from "./style.module.css";
 
 export const LoadAnimation = () => {
-	const [periods, setPeriods] = useState("");
-
-	useEffect(() => {
-		let numIntervals = 0;
-		setInterval(() => {
-			numIntervals++;
-			setPeriods(() => {
-				if (numIntervals == 1) {
-					return ".";
-				} else if (numIntervals == 2) {
-					return "..";
-				} else {
-					numIntervals = 0;
-					return "...";
-				}
-			});
-		}, 1000);
-	}, []);
-
 	return (
 		<div className={style.loadAnimation}>
-			<div>
-				<p>Retrieving Data{periods}</p>
-				<div className={style.loadBar}></div>
+			<div className={style.outerCircle}>
+				<div className={style.ball}></div>
 			</div>
 		</div>
 	);
